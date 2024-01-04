33d10e78e32616577e4dc545bb4e9a915cdacc00
Lucas Salicano <lucassalicano@hotmail.com>
2024-01-03 21:50:55 -0300

    feat: add liveness

--- server.go
+++ server.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/", Hello)
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
@@ -15,4 +19,13 @@ func main() {
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Kubernetes"))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt).Seconds()
+	if duration > 25 {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
 }
